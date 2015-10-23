0ac70581ea52cd372de74d1b5dfa4707b47bdc3b
calvins <calvins.biz@gmail.com>
2015-10-23 11:30:42 -0400

    Fixes

--- ui.R
+++ ui.R
@@ -4,24 +4,22 @@ shinyUI(
     pageWithSidebar(
         headerPanel('Predict Age of Abalone'),
         sidebarPanel(
+            img(src="AbaloneMeatByJackLikins.jpg",height=175,width=245),
+            a("Documentation", href="doc.html", target="_blank"),
             h3('Inputs'),
-            radioButtons("infantNonInfant", label = "Infant/Non-infant",
+            radioButtons("infantNonInfant", label = "Infant/Not-Infant",
                          choices = list("Infant" = 1, "Non-Infant" = 2), selected = 1),
-            numericInput('meanSize','Mean Size',NULL,min=0.1, max=1, step=0.1),
-            numericInput('shuckedWeight','Shucked Weight',NULL,min=0.1, max=1.5, step=0.1),
-            numericInput('shellWeight','Shell Weight',NULL,min=0.1, max=1.1, step=0.1),
+            numericInput('meanSize','Mean Size (0.1 to 1)',NULL,min=0.1, max=1, step=0.1),
+            numericInput('shuckedWeight','Shucked Weight (0.1 to 1.5)',NULL,min=0.1, max=1.5, step=0.1),
+            numericInput('shellWeight','Shell Weight (0.1 to 1.1)',NULL,min=0.1, max=1.1, step=0.1),
             actionButton('goButton','Go!')
         ),
         mainPanel(
-            h4('You entered'),
-            verbatimTextOutput("inputInfant"),
-            verbatimTextOutput("inputMeanSize"),
-            verbatimTextOutput("inputShuckedWeight"),
-            verbatimTextOutput("inputShellWeight"),
-            h4('Which resulted in a prediction of '),
+            h4('Predicted age'),
             verbatimTextOutput("prediction"),
             h4('Your fortune'),
             verbatimTextOutput("fortune"),
+            h4('Your abalone'),
             plotOutput("newRings")
         )
     )
